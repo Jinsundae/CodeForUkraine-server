9f395fe9528c0c66452ffad30b0de5c090096007
seungmin Ji <cswcsm02@gmail.com>
2022-03-06 13:23:43 +0900

    initial commit

---
+++ codeForUkraine/src/main/java/jinsundae/codeForUkraine/CodeForUkraineApplication.java
@@ -0,0 +1,13 @@
+package jinsundae.codeForUkraine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeForUkraineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeForUkraineApplication.class, args);
+	}
+
+}
