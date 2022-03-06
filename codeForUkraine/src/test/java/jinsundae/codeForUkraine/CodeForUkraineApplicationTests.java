b9b23e98fc2b54acdb0e7b170f663a9a663cf55f
seungmin Ji <cswcsm02@gmail.com>
2022-03-06 13:23:43 +0900

    initial commit

---
+++ codeForUkraine/src/test/java/jinsundae/codeForUkraine/CodeForUkraineApplicationTests.java
@@ -0,0 +1,13 @@
+package jinsundae.codeForUkraine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeForUkraineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
