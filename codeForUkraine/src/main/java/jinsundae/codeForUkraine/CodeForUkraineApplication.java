4696db4b2c98a2463616293de15b2afabc3b78cb
Gseungmin <87487149+Gseungmin@users.noreply.github.com>
2022-03-06 16:16:36 +0900

    db connection (#2)
    * initial commit
    
    * DB Connection

--- codeForUkraine/src/main/java/jinsundae/codeForUkraine/CodeForUkraineApplication.java
+++ codeForUkraine/src/main/java/jinsundae/codeForUkraine/CodeForUkraineApplication.java
@@ -1,13 +1,25 @@
 package jinsundae.codeForUkraine;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class CodeForUkraineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeForUkraineApplication.class, args);
 	}
-
+  
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }
